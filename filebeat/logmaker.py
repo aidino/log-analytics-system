c122fb12e8de514aa504beb4b3eb48a580e66a62
Ngo Hong Thai <ngohongthai.uet@gmail.com>
2023-08-21 00:38:18 +0700

    update code

--- filebeat/logmaker.py
+++ filebeat/logmaker.py
@@ -1,15 +1,16 @@
 import time
 
 def main():
-    input_file_path = "../data/raw/BGL_test1.log"
+    input_file_path = "../data/raw/BGL_test2.log"
     output_file_path = "log/output.log"
     
-
+    print("Start writing to output file...")
     with open(input_file_path, "r") as input_file, open(output_file_path, "w") as output_file:
         for line in input_file:
             output_file.write(line)
             output_file.flush()  # Flush the buffer to ensure immediate writing
-            time.sleep(1)  # Wait for 1 second
+            time.sleep(0.1)  # Wait for 1 second
+        print("Done")
 
 if __name__ == "__main__":
     main()
